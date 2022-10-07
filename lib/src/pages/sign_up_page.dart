36a3efb9fb2e96a8b82ffa9f465e522999a05203
IvoCobos <ivancho3110@hotmail.com>
2022-10-07 16:16:34 -0500

    Add sign_up_page

--- lib/src/pages/sign_up_page.dart
+++ lib/src/pages/sign_up_page.dart
@@ -26,7 +26,22 @@ class SignUpPage extends StatelessWidget {
                 color: Theme.of(context).primaryColor,
                 fontWeight: FontWeight.bold,
                 fontSize: 30.0)),
-            _username(context)
+            _usernameInput(context),
+            _emailInput(context),
+            _phoneInput(context),
+            _dateOfBirthInput(context),
+            _passwordInput(context),
+            _singUpButton(context),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Text(
+                'Al hacer click en "Crear cuenta" usted acepta los Terminos y Condiciones.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 13.0))
+              )
             ],
           ),
         ),
@@ -35,15 +50,118 @@ class SignUpPage extends StatelessWidget {
   }
 }
 
-Widget _username(BuildContext context){
+Widget _usernameInput(BuildContext context){
   return Container(
     margin: EdgeInsets.only(top: 40.0),
+    padding: EdgeInsets.only(left: 20.0),
     decoration: BoxDecoration(
       color: bgInput,
       borderRadius: BorderRadius.circular(40.0)),
     child: TextField(
       keyboardType: TextInputType.text,
-      decoration: InputDecoration,
+      decoration: InputDecoration(
+        hintText: 'Usuario',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
     ),
+  );
+}
+
+Widget _emailInput(BuildContext context){
+  return Container(
+    margin: EdgeInsets.only(top: 10.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: bgInput,
+      borderRadius: BorderRadius.circular(40.0)),
+    child: TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'email',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _phoneInput(BuildContext context){
+  return Container(
+    margin: EdgeInsets.only(top: 10.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: bgInput,
+      borderRadius: BorderRadius.circular(40.0)),
+    child: TextField(
+      keyboardType: TextInputType.phone,
+      decoration: InputDecoration(
+        hintText: 'Tele´fono',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _dateOfBirthInput(BuildContext context){
+  return Container(
+    margin: EdgeInsets.only(top: 10.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: bgInput,
+      borderRadius: BorderRadius.circular(40.0)),
+    child: TextField(
+      keyboardType: TextInputType.datetime,
+      decoration: InputDecoration(
+        hintText: 'Fecha de nacimiento',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _passwordInput(BuildContext context){
+  return Container(
+    margin: EdgeInsets.only(top: 10.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: bgInput,
+      borderRadius: BorderRadius.circular(40.0)),
+    child: TextField(
+      keyboardType: TextInputType.visiblePassword,
+      decoration: InputDecoration(
+        hintText: 'Contraseña',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _singUpButton(BuildContext context){
+  return Container(
+    width: 370.0,
+    height: 45.0,
+    margin: EdgeInsets.only(top: 30.0),
+    child: RaisedButton(
+      onPressed: () {
+
+      },
+      color:  Theme.of(context).accentColor,
+      child: Text(
+       'Crear cuenta',
+       style: TextStyle(color: Colors.white, fontSize: 17.0),
+       ),
+       shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0)
+        ),
+      ),
   );
 }
