0ce65d505ef7807badae1c582e5cc4e31f8b01ad
IvoCobos <ivancho3110@hotmail.com>
2022-11-09 17:28:35 -0500

    Add topbar widget

--- lib/src/features/presentation/tabs/explore_tab.dart
+++ lib/src/features/presentation/tabs/explore_tab.dart
@@ -1,13 +1,30 @@
+import 'package:delivery_app/src/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class ExploreTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Explore'),
-      ),
-    );
+    return SafeArea(child: CustomScrollView(
+      slivers: [SliverList(delegate: SliverChildListDelegate(
+        [
+          _topBar(context)
+      ]))],
+    ));
   }  
+}
+
+Widget _topBar(BuildContext context) {
+  return Row(
+    children: [
+      Container(
+        child: Row(children: [
+          Icon(Icons.search, size: 20.0, color: grey,)
+          ]),
+      ),
+      Container(
+        child: Text('Hola'),
+      )
+    ],
+  );
 }
