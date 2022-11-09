b5c7e1814a4f19efa52e7e6702d260156d11b113
IvoCobos <ivancho3110@hotmail.com>
2022-11-09 17:33:48 -0500

    Add search input

--- lib/src/features/presentation/tabs/explore_tab.dart
+++ lib/src/features/presentation/tabs/explore_tab.dart
@@ -18,8 +18,19 @@ Widget _topBar(BuildContext context) {
   return Row(
     children: [
       Container(
+        width: 310,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.only(left: 16),
+        decoration: BoxDecoration(
+          border: Border.all(color: Color.fromRGBO(234, 236, 239, 1.0)),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
         child: Row(children: [
-          Icon(Icons.search, size: 20.0, color: grey,)
+          Icon(Icons.search, size: 20.0, color: grey,),
+          Text('Search', style: TextStyle(
+            color: grey,
+            fontSize: 17.0
+          ))
           ]),
       ),
       Container(
